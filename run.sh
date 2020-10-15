16af98f80cfa0af7d87ae33a18a69efeb956b560
makeran218 <70996422+makeran218@users.noreply.github.com>
2020-10-16 01:38:52 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 find '/content/drive/'  -type d -name ".unwanted" -exec rm -rf "{}" \;
 find '/content/drive/' -type d -empty -delete;
-rclone move "$1" "baros2-campur:/Torrents/$2" --checkers 20 --fast-list -v --tpslimit 20 --transfers 20 --exclude '.unwanted/' --delete-empty-src-dirs
+rclone move "$1" "baros-campur:/Torrents/$2" --checkers 20 --fast-list -v --tpslimit 20 --transfers 20 --exclude '.unwanted/' --delete-empty-src-dirs
 

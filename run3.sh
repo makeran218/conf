d8033c982627b300647931c7dd7270b24f635189
makeran218 <70996422+makeran218@users.noreply.github.com>
2020-10-18 14:25:24 +0700

    Update run3.sh

--- run3.sh
+++ run3.sh
@@ -1,3 +1,3 @@
 find '/content/drive/'  -type d -name ".unwanted" -exec rm -rf "{}" \;
 find '/content/drive/' -type d -empty -delete;
-rclone move "$1" "baros3-campur:/Torrents/$2" --checkers 20 --log-file=/content/rclone.log --fast-list -v --tpslimit 20 --transfers 20 --exclude '.unwanted/' --delete-empty-src-dirs
+rclone move "$1" "baros3-campur:/Torrents/${2%.*}" --checkers 20 --log-file=/content/rclone.log --fast-list -v --tpslimit 20 --transfers 20 --exclude '.unwanted/' --delete-empty-src-dirs

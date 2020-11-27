7ed05d03bfe1db8990ef0763abdfcfbf48a64816
makeran218 <70996422+makeran218@users.noreply.github.com>
2020-11-27 23:20:18 +0700

    Update run3.sh

--- run3.sh
+++ run3.sh
@@ -1,2 +1,2 @@
-
-rclone move "$1" "baros3-campur:/Torrents/${2%.*}" --checkers 20 --log-file=/content/rclone.log --fast-list -v --tpslimit 20 --transfers 20 --exclude '.unwanted/' --delete-empty-src-dirs
+#!/bin/bash
+rclone move "$3/$2" "baros3-campur:/Torrents/${2%.*}" --checkers 20 --log-file=/content/rclone.log --fast-list -v --tpslimit 20 --transfers 20 --exclude '.unwanted/' --delete-empty-src-dirs
